6c8d7a1bb79bc5e9db26ba30b0b2caba02bd6b13
anees8 <aneesahmed8484@gmail.com>
2022-09-09 19:12:45 +0530

    Users Table

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,13 +3,14 @@ import Home from './components/Home.vue';
 import About from './components/About.vue';
 import Settings from './components/Settings.vue';
 import NotFoundComponent from './components/404.vue';
+import Users from './components/Users.vue';
 
 const routes = [
   {path:'/',component:Home},
   {path:'/about',component:About},
   {path:'/settings',component:Settings},
   {path:'/logout',component:Home},
-  {path:'/list_users',component:Home},
+  {path:'/list_users',component:Users},
   {path:'/list_employee',component:Home},
   {path:'/list_outsource_employee',component:Home},
   {path:'/list_customer',component:Home},
